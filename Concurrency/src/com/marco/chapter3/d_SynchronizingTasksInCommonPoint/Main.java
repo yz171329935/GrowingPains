925f575eba494fdc6726e2d43f470c97a5bc9106
yaozhen <zhen.yao.sh@dianping.com>
2017-03-07 10:48:34 +0800

    add comment

--- Concurrency/src/com/marco/chapter3/d_SynchronizingTasksInCommonPoint/Main.java
+++ Concurrency/src/com/marco/chapter3/d_SynchronizingTasksInCommonPoint/Main.java
@@ -1,15 +1,20 @@
 package com.marco.chapter3.d_SynchronizingTasksInCommonPoint;
 
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created by marco on 2017/2/3.
  * CyclicBarrier 情景:
  * 等所有的操作都执行完毕以后 最后再执行某个实现了Runnable的操作
+ * 因为使用CyclicBarrier的线程都会阻塞在await方法上，所以在线程池中使用时要特别小心，如果线程池过小，那么久会发生死锁了
  */
 public class Main {
 
-    public static void main(String[] args) {
+    private static ExecutorService es = Executors.newFixedThreadPool(5);
+
+    public static void main(String[] args) throws Exception{
         final int ROWS = 10000;
         final int NUMBERS = 1000;
         final int SEARCH = 5;
@@ -27,10 +32,9 @@ public class Main {
         Searcher searchers[] = new Searcher[PARTICIPANTS];
         for (int i = 0; i < PARTICIPANTS; i++) {
             searchers[i] = new Searcher(i * LINES_PARTICIPANT, (i * LINES_PARTICIPANT) + LINES_PARTICIPANT, mock, results, 5, barrier);
-            Thread thread = new Thread(searchers[i]);
-            thread.start();
+            es.submit(searchers[i]);
         }
-
+        Thread.sleep(2000000);
         System.out.printf("Main: The main thread has finished.\n");
     }
 }
